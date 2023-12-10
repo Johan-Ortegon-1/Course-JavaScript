9bf5e0361c0f29999b4636161b092a7b24125b52
Johan Ortegon 1 <johandanielortegonparra@gmail.com>
2023-12-10 00:01:14 -0500

    Objects and clasees done

--- 8-Objects/app.js
+++ 8-Objects/app.js
@@ -44,3 +44,40 @@ let nestedObject = {
 let { code : cod, subObject : {name : nam, subSubObject : doubleSub } } = nestedObject;
 
 console.log(doubleSub);
+
+// Class
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    getGeneralInfo() {
+        let info = `${this.name} and age: ${this.age}`
+        return info;
+    }
+}
+
+const person1 = new Person('pepe', 34);
+
+console.log(person1.getGeneralInfo());
+
+// Inheritance
+
+class Employee extends Person {
+    constructor(name, age, salary, job) {
+        super(name, age);
+        this.salary = salary;
+        this.job = job;
+    }
+
+    getGeneralInfo() {
+        return super.getGeneralInfo() + ` Salary: ${this.salary} Job: ${this.job}`;
+    }
+}
+
+const employee1 = new Employee('pepe segundo', 54, 12345, 'Boss pepe');
+
+console.log(employee1.getGeneralInfo());
+
